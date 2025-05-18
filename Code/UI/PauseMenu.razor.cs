0fc863c61be889f9b8d6c93c45d8f4b70a3d5fff
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-18 23:08:03 +0300

    z-indexing

--- Code/UI/PauseMenu.razor.cs
+++ Code/UI/PauseMenu.razor.cs
@@ -6,7 +6,6 @@ namespace CardGame.UI;
 public partial class PauseMenu
 {
 	private SettingsPanel? _settingsPanel;
-	private Panel? _main;
 	
 	private static SaveManager? SaveManager => CardGame.SaveManager.Instance;
 	private static SceneManager? SceneManager => CardGame.SceneManager.Instance;
