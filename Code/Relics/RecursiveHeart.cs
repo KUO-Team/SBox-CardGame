5df5351095a6a4de5379c8dc07b70fa37248107c
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-18 17:10:43 +0300

    RecursiveHeart proper implementation

--- Code/Relics/RecursiveHeart.cs
+++ Code/Relics/RecursiveHeart.cs
@@ -4,9 +4,22 @@ namespace CardGame.Relics;
 
 public class RecursiveHeart( Data.Relic data ) : Relic( data )
 {
+	private readonly Dictionary<BattleUnit, Card> _playedCards = new();
+	
 	public override void AfterPlayCard( Card card, BattleUnit unit )
 	{
+		if ( !_playedCards.TryAdd( unit, card ) )
+		{
+			return;
+		}
+		
 		unit.HandComponent?.Draw( card, true );
 		base.AfterPlayCard( card, unit );
+	}
+
+	public override void OnTurnEnd()
+	{
+		_playedCards.Clear();
+		base.OnTurnEnd();
 	}
 }
