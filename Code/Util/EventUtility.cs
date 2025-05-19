9ae68f49337d2748bd6d6ddb06e9029e5d78d212
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-20 00:36:33 +0300

    EventUtility improvements

--- Code/Util/EventUtility.cs
+++ Code/Util/EventUtility.cs
@@ -1,8 +1,11 @@
-﻿namespace CardGame;
+﻿using System;
+using CardGame.Data;
+
+namespace CardGame;
 
 public static class EventUtility
 {
-	public static void GiveMoney( int amount )
+	public static void AddMoney( int amount )
 	{
 		var player = Player.Local;
 		if ( !player.IsValid() )
@@ -11,5 +14,50 @@ public static class EventUtility
 		}
 		
 		player.Money += amount;
+	}
+	
+	public static void SubtractMoney( int amount )
+	{
+		var player = Player.Local;
+		if ( !player.IsValid() )
+		{
+			return;
+		}
+		
+		player.Money = Math.Max( 0, player.Money - amount );
+	}
+
+	public static void AddCard( Id id )
+	{
+		var player = Player.Local;
+		if ( !player.IsValid() )
+		{
+			return;
+		}
+		
+		var card = CardDataList.GetById( id );
+		if ( card is not null )
+		{
+			PlayerData.Data.SeeCard( id );
+			player.Cards.Add( card );
+		}
+		else
+		{
+			Log.Warning( $"Unable to add card: no card with ID {id} found!" );
+		}
+	}
+
+	public static void AddRelic( Id id )
+	{
+		var relic = RelicDataList.GetById( id );
+		if ( relic is not null )
+		{
+			PlayerData.Data.SeeRelic( id );
+			RelicManager.Instance?.AddRelic( relic );
+		}
+		else
+		{
+			Log.Warning( $"Unable to add relic: no relic with ID {id} found!" );
+		}
 	}
 }
