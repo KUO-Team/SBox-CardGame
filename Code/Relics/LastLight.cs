46dfc261f216262be0ec3d6096870ff926f5a91f
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-21 15:54:03 +0300

    LastLight actually functions

--- Code/Relics/LastLight.cs
+++ Code/Relics/LastLight.cs
@@ -1,6 +1,37 @@
-﻿namespace CardGame.Relics;
+﻿using CardGame.Data;
+using CardGame.Units;
+
+namespace CardGame.Relics;
 
 public class LastLight( Data.Relic data ) : Relic( data )
 {
+	private bool _triggered;
 	
+	public override void OnTakeDamage( int damage, BattleUnit target, BattleUnit? attacker = null )
+	{
+		if ( !target.HealthComponent.IsValid() )
+		{
+			return;
+		}
+
+		if ( _triggered )
+		{
+			return;
+		}
+
+		if ( damage >= target.HealthComponent.Health )
+		{
+			var halfOfMax = target.HealthComponent.MaxHealth / 2;
+			target.HealthComponent.Health = halfOfMax;
+			_triggered = true;
+		}
+		
+		base.OnTakeDamage( damage, target, attacker );
+	}
+
+	public override void OnBattleStart( Battle battle )
+	{
+		_triggered = false;
+		base.OnBattleStart( battle );
+	}
 }
