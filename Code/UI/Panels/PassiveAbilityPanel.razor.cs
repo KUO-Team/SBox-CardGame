b61fd69d5a5723db6533b9807b92261bddf744a1
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-24 00:04:46 +0300

    Add PassiveAbilityPanel

---
+++ Code/UI/Panels/PassiveAbilityPanel.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CardGame.Data;
+
+namespace CardGame.UI;
+
+public partial class PassiveAbilityPanel
+{
+	[Parameter]
+	public PassiveAbility? Passive { get; set; }
+
+	protected override int BuildHash()
+	{
+		return HashCode.Combine( Passive );
+	}
+}
