8e860414f5eec925047a05c7837dbe64343d4805
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-23 20:55:12 +0300

    Remove CardSlot from EchoOfOne

--- Code/Relics/EchoOfOne.cs
+++ Code/Relics/EchoOfOne.cs
@@ -1,9 +1,9 @@
-﻿namespace CardGame.Relics;
+﻿using CardGame.Data;
+
+namespace CardGame.Relics;
 
 public class EchoOfOne( Data.Relic data ) : Relic( data )
 {
-	private bool _added;
-	
 	public override void OnTurnStart()
 	{
 		if ( !Owner.IsValid() || !Owner.HealthComponent.IsValid() || !Owner.Slots.IsValid() )
@@ -22,15 +22,6 @@ public class EchoOfOne( Data.Relic data ) : Relic( data )
 		}
 		
 		Owner.StatusEffects?.AddStatusEffectByKey( StatusEffects.StatusEffect.StatusKey.PowerUp, 3 );
-
-		if ( _added )
-		{
-			return;
-		}
-
-		Owner.Slots.AddCardSlot( 1 );
-		_added = true;
-		
 		base.OnTurnStart();
 	}
 }
