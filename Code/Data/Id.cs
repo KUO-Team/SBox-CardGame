e57889f5b99aded66bbc89e4ef49cc3aa31cb457
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-24 17:57:52 +0300

    Id implements IComparable<Id>

--- Code/Data/Id.cs
+++ Code/Data/Id.cs
@@ -3,33 +3,47 @@ using System.Text.Json.Serialization;
 
 namespace CardGame.Data;
 
-public sealed class Id : IEquatable<Id>
+public sealed class Id : IEquatable<Id>, IComparable<Id>
 {
 	/// <summary>
 	/// If this resource comes from a mod, this is usually the ident of the mod.
 	/// </summary>
 	public string Source { get; init; } = string.Empty;
-	
+
 	public int LocalId { get; init; }
- 	
+
 	[JsonIgnore, Hide]
 	public bool IsFromMod => Source.Length > 0;
-	
+
 	[JsonIgnore, Hide]
 	public bool IsValid => LocalId >= 0;
 
-	public static Id Invalid => new() { LocalId = InvalidId };
+	public static Id Invalid => new()
+	{
+		LocalId = InvalidId
+	};
 
 	private const int InvalidId = -1;
-	
+
 	public bool Equals( Id? other )
 	{
 		return other is not null && other.Source == Source && other.LocalId == LocalId;
 	}
-	
+
 	public override bool Equals( object? obj )
 	{
 		return Equals( obj as Id );
+	}
+
+	public int CompareTo( Id? other )
+	{
+		if ( other is null )
+		{
+			return 1;
+		}
+
+		var sourceComparison = string.Compare( Source, other.Source, StringComparison.Ordinal );
+		return sourceComparison != 0 ? sourceComparison : LocalId.CompareTo( other.LocalId );
 	}
 
 	public override int GetHashCode()
@@ -43,5 +57,8 @@ public sealed class Id : IEquatable<Id>
 	}
 
 	public static implicit operator int( Id id ) => id.LocalId;
-	public static implicit operator Id( int id ) => new() { LocalId = id };
+	public static implicit operator Id( int id ) => new()
+	{
+		LocalId = id
+	};
 }
