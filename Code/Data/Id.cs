41fb7d07b315e4f6f352bbe3da6c6e9c19a88136
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-24 19:02:20 +0300

    Id is a record

--- Code/Data/Id.cs
+++ Code/Data/Id.cs
@@ -3,37 +3,15 @@ using System.Text.Json.Serialization;
 
 namespace CardGame.Data;
 
-public sealed class Id : IEquatable<Id>, IComparable<Id>
+public sealed record Id( string Source, int LocalId ) : IComparable<Id>
 {
-	/// <summary>
-	/// If this resource comes from a mod, this is usually the ident of the mod.
-	/// </summary>
-	public string Source { get; init; } = string.Empty;
-
-	public int LocalId { get; init; }
-
 	[JsonIgnore, Hide]
 	public bool IsFromMod => Source.Length > 0;
 
 	[JsonIgnore, Hide]
 	public bool IsValid => LocalId >= 0;
 
-	public static Id Invalid => new()
-	{
-		LocalId = InvalidId
-	};
-
-	private const int InvalidId = -1;
-
-	public bool Equals( Id? other )
-	{
-		return other is not null && other.Source == Source && other.LocalId == LocalId;
-	}
-
-	public override bool Equals( object? obj )
-	{
-		return Equals( obj as Id );
-	}
+	public static Id Invalid => new( string.Empty, -1 );
 
 	public int CompareTo( Id? other )
 	{
@@ -45,20 +23,12 @@ public sealed class Id : IEquatable<Id>, IComparable<Id>
 		var sourceComparison = string.Compare( Source, other.Source, StringComparison.Ordinal );
 		return sourceComparison != 0 ? sourceComparison : LocalId.CompareTo( other.LocalId );
 	}
-
-	public override int GetHashCode()
-	{
-		return HashCode.Combine( Source, LocalId );
-	}
-
+	
 	public override string ToString()
 	{
 		return IsFromMod ? $"{Source} : {LocalId}" : LocalId.ToString();
 	}
 
 	public static implicit operator int( Id id ) => id.LocalId;
-	public static implicit operator Id( int id ) => new()
-	{
-		LocalId = id
-	};
+	public static implicit operator Id( int id ) => new( string.Empty, id );
 }
