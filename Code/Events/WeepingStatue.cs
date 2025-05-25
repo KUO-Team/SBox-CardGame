724cc06ec7eef9c9f640a8959424ea035b830d87
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-25 23:18:03 +0300

    Remove the hp penalty of weepingstatue event

--- Code/Events/WeepingStatue.cs
+++ Code/Events/WeepingStatue.cs
@@ -3,29 +3,6 @@
 public class WeepingStatue( Data.Event data ) : Event( data )
 {
 	private const int RelicId = 100;
-	
-	public override void OnShow( Data.Event @event )
-	{
-		var player = Player.Local;
-		if ( !player.IsValid() )
-		{
-			return;
-		}
-
-		var unit = player.Unit;
-		if ( unit is null )
-		{
-			return;
-		}
-
-		var takeChoice = @event.Choices[0];
-		if ( unit.Hp <= 10 )
-		{
-			takeChoice.Enabled = false;
-		}
-
-		base.OnShow( @event );
-	}
 
 	public override void OnChoiceSelected( Data.Event.Choice choice, int index )
 	{
@@ -43,10 +20,7 @@ public class WeepingStatue( Data.Event data ) : Event( data )
 		switch ( index )
 		{
 			case 0:
-				{
-					Player.Local?.Unit?.Damage( 10 );
-					EventUtility.AddRelic( RelicId );
-				}
+				EventUtility.AddRelic( RelicId );
 				break;
 			case 1:
 				break;
