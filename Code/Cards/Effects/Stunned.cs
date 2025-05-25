3b60d2076900515b7f165e21dbc6496f3677825c
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-05-26 01:10:10 +0300

    Add Stun card effects

---
+++ Code/Cards/Effects/Stunned.cs
@@ -0,0 +1,12 @@
+﻿namespace CardGame.Effects;
+
+public class Stunned( Card card ) : CardEffect( card )
+{
+	public override string Description => "Become Stunned";
+
+	public override void OnPlay( CardEffectDetail detail )
+	{
+		detail.Unit?.StatusEffects?.AddStatusEffectByKey( StatusEffects.StatusEffect.StatusKey.Stunned );
+		base.OnPlay( detail );
+	}
+}
---
+++ Code/Cards/Effects/StunnedNextTurn.cs
@@ -0,0 +1,12 @@
+﻿namespace CardGame.Effects;
+
+public class StunnedNextTurn( Card card ) : CardEffect( card )
+{
+	public override string Description => "Become Stunned Next Turn";
+	
+	public override void OnPlay( CardEffectDetail detail )
+	{
+		detail.Unit?.StatusEffects?.AddStatusEffectByKeyNextTurn( StatusEffects.StatusEffect.StatusKey.Stunned );
+		base.OnPlay( detail );
+	}
+}
