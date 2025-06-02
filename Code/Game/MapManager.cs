9cc3da5cebe00876cbb446009653f3dccb95cb5f
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-06-02 15:07:22 +0300

    Add Shop event

--- Code/Game/MapManager.cs
+++ Code/Game/MapManager.cs
@@ -55,16 +55,16 @@ public sealed class MapManager : Singleton<MapManager>
 	public Dictionary<int, List<Id>> FloorEvents { get; set; } = new()
 	{
 		{
-			3, [1]
+			3, [1, 2]
 		},
 		{
-			2, [1, 2, 3]
+			2, [1, 2, 3, 4]
 		},
 		{
-			1, [1, 2, 3]
+			1, [1, 2, 3, 4]
 		},
 		{
-			0, [1, 2, 3]
+			0, [1, 2, 3, 4]
 		}
 	};
 
