6520da4325cf6328b0b885b7734ca1d64cc62e51
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-06-04 01:42:11 +0300

    Try to fix PowerInHand

--- Code/Cards/Effects/PowerInHand.cs
+++ Code/Cards/Effects/PowerInHand.cs
@@ -1,11 +1,46 @@
-﻿namespace CardGame.Effects;
+﻿using CardGame.Modifiers;
+
+namespace CardGame.Effects;
 
 public class PowerInHand( Card card ) : CardEffect( card )
 {
-	public override string Description => $"Deal damage equal to the amount of cards named {Card.Name} in hand";
+	public override string Description => $"Gain power equal to the amount of cards named {Card.Name} in hand";
 
-	public override int DamageModifier( CardEffectDetail detail )
+	private PowerModifier? _modifier;
+
+	public override void BeforePlay( CardEffectDetail detail )
 	{
-		return detail.Unit?.HandComponent?.Hand.Count( x => x.Name == Card.Name ) ?? 0;
+		if ( !detail.Unit.IsValid() )
+		{
+			return;
+		}
+
+		if ( !detail.Unit.HandComponent.IsValid() )
+		{
+			return;
+		}
+		
+		var amount = detail.Unit.HandComponent.Hand.Count( x => x.Name == Card.Name );
+		if ( amount <= 0 )
+		{
+			return;
+		}
+
+		_modifier = new PowerModifier( amount, action => action.Type == Action.ActionType.Attack, 1 );
+		Card.Modifiers.AddModifier( _modifier );
+		
+		base.BeforePlay( detail );
+	}
+
+	public override void OnPlay( CardEffectDetail detail )
+	{
+		if ( _modifier is null )
+		{
+			return;
+		}
+		
+		Card.Modifiers.RemoveModifier( _modifier );
+		_modifier = null;
+		base.OnPlay( detail );
 	}
 }
