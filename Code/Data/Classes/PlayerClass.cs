7cc7ffecafdb5935e3941326b92534a63d82c12b
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-06-05 19:42:44 +0300

    Add PlayerClass.Relics, PlayerClass.IsUnlocked

--- Code/Data/Classes/PlayerClass.cs
+++ Code/Data/Classes/PlayerClass.cs
@@ -1,6 +1,6 @@
 ﻿namespace CardGame.Data;
 
-public class PlayerClass : IResource
+public class PlayerClass : IResource, IDeepCopyable<PlayerClass>
 {
 	[InlineEditor]
 	public Id Id { get; set; } = Id.Invalid;
@@ -11,4 +11,22 @@ public class PlayerClass : IResource
 	
 	[InlineEditor]
 	public Id Unit { get; set; } = Id.Invalid;
+
+	[InlineEditor]
+	public List<Id> Relics { get; set; } = [];
+	
+	public bool IsUnlocked { get; set; } = true;
+	
+	public PlayerClass DeepCopy()
+	{
+		return new PlayerClass
+		{
+			Id = Id,
+			Name = Name,
+			Description = Description,
+			Unit = Unit,
+			Relics = [..Relics],
+			IsUnlocked = IsUnlocked
+		};
+	}
 }
