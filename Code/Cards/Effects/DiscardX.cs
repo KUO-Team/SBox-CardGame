f318222d792ebaf899d6074652df20027e5dc656
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-06-08 14:30:02 +0300

    Add DiscardX card effect

---
+++ Code/Cards/Effects/DiscardX.cs
@@ -0,0 +1,12 @@
+﻿namespace CardGame.Effects;
+
+public class DiscardX( Card card, RangedInt power ) : CardEffect( card, power )
+{
+	public override string Description => PowerRange.Max == 1 ? "Discard 1 Card" : "Discard @ Cards";
+
+	public override void OnPlay( CardEffectDetail detail )
+	{
+		detail.Unit?.HandComponent?.EnterDiscardMode( Card );
+		base.OnPlay( detail );
+	}
+}
